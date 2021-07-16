31c6ee85af1b8642860f352ce457eace2452c37f
Shreya Sur <68423972+shreyasur@users.noreply.github.com>
2021-07-16 19:10:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import  requests
+import requests
+
+similarity_matrix = pickle.load(open('model/similarity_matrix.pkl', 'rb'))
+
 
 def fetch_poster(movie_id):
-    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=e8286ea906682bbd896ec7e2972d563d&language=en-US'.format(movie_id))
-
-    data=response.json()
+    response= requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
+    data = response.json()
+   
 
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
@@ -16,32 +19,32 @@ def recommend(movie_name):
 
     L = sorted(list(enumerate(similarity_matrix[movie_index])), reverse=True, key=lambda x: x[1])
 
-    recommended_movies=[]
-    recommended_movies_posters=[]
-
+    recommended_movies = []
+    recommended_movies_posters = []
 
     for i in L[1:6]:
-        movie_id=movies.iloc[i[0]]['id']
+        movie_id = movies.iloc[i[0]]['id']
 
         recommended_movies.append(movies.iloc[i[0]]['original_title'])
         # fetch poster from API
         recommended_movies_posters.append(fetch_poster(movie_id))
 
-    return recommended_movies,recommended_movies_posters
+    return recommended_movies, recommended_movies_posters
 
-similarity_matrix=pickle.load(open('similarity_matrix.pkl','rb'))
 
-movies_dict=pickle.load(open('movie_dict.pkl','rb'))
-movies=pd.DataFrame(movies_dict)
+
+
+movies_dict = pickle.load(open('model/movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
 
 st.title('Movie Recommender System')
 
-selected_movie_name=st.selectbox('Which movie have you recently watched?',
-                    movies['original_title'].values)
+selected_movie_name = st.selectbox('Which movie have you recently watched?',
+                                   movies['original_title'].values)
 
-if st.button('Recommend'):
-    names,posters=recommend(selected_movie_name)
-    col1,col2,col3,col4,col5=st.beta_columns(5)
+if st.button('Show Recommendation'):
+    names, posters = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
     with col1:
         st.text(names[0])
         st.image(posters[0])
